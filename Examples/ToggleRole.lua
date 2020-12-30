cf15c7ceb7f5c19e8b08c5d779afe8454406d2e7
Sythivo <69376875+Sythivo@users.noreply.github.com>
2020-12-30 13:22:50 -0500

    Create ToggleRole.lua

---
+++ Examples/ToggleRole.lua
@@ -0,0 +1,26 @@
+--Imposter/Crewmate Toggle
+-- Switches Between Imposter and Crewmate
+-- By Sythivo
+
+--Integer
+-- Gets the Role of Player
+local CurrentRole = AmongUs.Player.GetRole()
+
+-- Check if Variable is Set or !Null
+if (CurrentRole) then
+
+  -- Check if Variable is Role.Imposter | 1
+	if (CurrentRole == Role.Imposter) then
+    
+    -- Set Player's Role to {Role.Crewmate}
+		AmongUs.Player.SetRole(Role.Crewmate)
+    
+  -- Check if Variable is Role.Crewmate | 0
+	elseif (CurrentRole == Role.Crewmate) then
+  
+    -- Set Player's Role to {Role.Imposter}
+		AmongUs.Player.SetRole(Role.Imposter)
+    
+	end
+  
+end
