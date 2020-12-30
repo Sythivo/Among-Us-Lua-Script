16cd2e838c207ce585d40f938f6cc84fa7492ee0
Sythivo <69376875+Sythivo@users.noreply.github.com>
2020-12-30 00:36:23 -0500

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,21 @@
+--[[
+ Change Players Role <- Fuction
+     Role.Imposter : Role.Crewmate
+--]]
+AmongUs.Player.SetRole(Role.Imposter)
+-- Readable Type
+AmongUs.Player.GetRole()
+
+--[[
+ Change Players Status <- Readable
+--]]
+AmongUs.Player.IsAlive = true
+
+--[[
+ Change Players Clip <- Function
+--]]
+AmongUs.Player.Clip(false)
+
+--[[
+ More Soon...
+--]]
